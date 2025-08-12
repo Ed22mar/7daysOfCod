70cbde876f950e166d7f53f7d94c9263539d4651
Ed22mar <edgaricloud@gmail.com>
2025-08-12 23:56:38 +0100

    adicionar as codições de teste  e finalização do projecto

--- app.js
+++ app.js
@@ -9,9 +9,20 @@ let stringDez = '10';
 let numeroTrinta = 30;
 let stringTrinta = '30';
 
-if(numeroUm === stringUm){
-  console.log("Teste");
-}else{
-  console.log("dd");
+if (numeroUm === stringUm) {
+  console.log('As variáveis numeroUm e stringUm têm o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroUm e stringUm não têm o mesmo valor')
 }
-  
+
+if (numeroTrinta === stringTrinta) {
+  console.log('As variáveis numeroTrinta e stringTrinta têm o mesmo valor e mesmo tipo')
+} else {
+  console.log('As variáveis numeroTrinta e stringTrinta não têm o mesmo tipo')
+}
+
+if (numeroDez === stringDez) {
+  console.log('As variáveis numeroDez e stringDez têm o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroDez e stringDez não têm o mesmo valor')
+}
