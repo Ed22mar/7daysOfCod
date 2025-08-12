d1a76e7b9869a66fadd8505e843f3bddf92b1f27
Ed22mar <edgaricloud@gmail.com>
2025-08-12 23:50:56 +0100

    declaração das variasveis para comparação

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("teste");
+// # Desafio 1 - Operações boleanas
+
+let numeroUm = 1;
+let stringUm = '1';
+
+let numeroDez = 10;
+let stringDez = '10';
+
+let numeroTrinta = 30;
+let stringTrinta = '30';
+
+if(numeroUm === stringUm){
+  console.log("Teste");
+}else{
+  console.log("dd");
+}
+  
